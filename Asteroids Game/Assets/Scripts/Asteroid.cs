69b4b120d2d5e952eacfd26331b4490e4cc273c8
hungtabe <trandinhhung717@gmail.com>
2025-02-13 14:58:29 +0700

    [Unity_2] Prefab Restore

--- Asteroids Game/Assets/Scripts/Asteroid.cs
+++ Asteroids Game/Assets/Scripts/Asteroid.cs
@@ -4,7 +4,15 @@ public class Asteroid : MonoBehaviour
 {
     public Sprite[] sprites;
 
-    //public 
+    public float size = 1.0f;
+
+    public float minSize = 0.5f;
+
+    public float maxSize = 1.5f;
+
+    public float speed = 50.0f;
+
+    public float maxLifetime = 30.0f;
 
     private SpriteRenderer _spriteRenderer;
 
@@ -21,6 +29,18 @@ public class Asteroid : MonoBehaviour
         _spriteRenderer.sprite = sprites[Random.Range(0, sprites.Length)];
 
         this.transform.eulerAngles = new Vector3(0.0f, 0.0f, Random.value * 360.0f);
-        this.transform.localScale = new Vector3()
+        this.transform.localScale = Vector3.one * this.size;
+
+        _rigidbody.mass = this.size;
     }
+
+    public void SetTrajectory(Vector2 direction)
+    {
+        _rigidbody.AddForce(direction * this.speed);
+
+        Destroy(this.gameObject, this.maxLifetime);
+    } 
+
 }
+
+

5385a7f8fb97a678ba206f61cd9a1c1a10429f9c
hungtabe <trandinhhung717@gmail.com>
2025-03-27 01:45:54 +0700

    [Unity_5] Game Manager Develop

---
+++ Asteroids Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public int lives = 3;
+    public float respawnTime = 3.0f;
+
+    public void PlayerDied()
+    {
+        this.lives--;
+        
+        if (this.lives <= 0)
+        {
+            GameOver();
+        } else { 
+        
+        Invoke(nameof(Respawn), this.respawnTime);
+
+        }
+
+    }
+
+    private void Respawn()
+    {
+        // // Đặt lại vị trí của người chơi về (0, 0, 0)
+        this.player.transform.position = Vector3.zero;
+        // // Kích hoạt lại đối tượng người chơi
+        this.player.gameObject.SetActive(true);
+
+    }
+
+    private void GameOver()
+    {
+
+    }
+}
